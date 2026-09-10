6055eadaaf3caaebf637d9ab55cabb25fab10c53
Victor Gustavo <victor.gustavo1982@gmail.com>
2026-09-10 11:13:23 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -5,15 +5,18 @@ import pandas as pd
 st.set_page_config(page_title="Dashboard Musical", page_icon="🎵", layout="wide")
 
 ## 1.Coloque o titulo do dashboard
-
+st.title('Playlist Musical')
 ## 2.Carregar os dados do arquivo 'Dados_Artistas.csv'
-df = pd.read_csv('Dados_Artistas.csv')
+df = pd.read_parquet('Dados_Artistas.parquet')
 
 # Único filtro, adicionando opção 'TODOS'
 artista = st.selectbox("Escolha um Artista:", ['TODOS'] + list(df['Artist'].unique()))
 
 ## 3.Filtrar os dados com base na seleção do artista
-
+if artista == 'TODOS':
+    dados = df
+else:
+    dados = df[df['Artist'] == artista]
 
 # Apenas 3 métricas
 st.write("### 📈 Métricas")
@@ -46,9 +49,10 @@ with container:
         st.line_chart(comparacao)
 
 ## 4. Escreva o título da tabela
-
+st.title("Top 10 Spotify")
 
 # Apenas ordenar por Stream e pegar os top 10
 top_10_spotify = dados.sort_values('Stream', ascending=False).head(10)[['Track', 'Artist', 'Stream']]
 
 ## 5. Mostre a tabela que criamos acima
+st.dataframe(top_10_spotify)
